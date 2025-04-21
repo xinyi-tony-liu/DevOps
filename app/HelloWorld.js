f811009c96192408b87d67f5375ff907dcaaf229
xinyi-tony-liu <lxy952@gmail.com>
2025-04-20 21:06:29 -0400

    Testing the workflow with a different message

--- app/HelloWorld.js
+++ app/HelloWorld.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello world!');
+  res.send('Hello world with GitHub Actions?');
 });
 
 app.listen(port, () => {
