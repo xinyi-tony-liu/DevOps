6f57eb38364ee9f3ff53579053460bd29597aae7
xinyi-tony-liu <lxy952@gmail.com>
2025-04-21 16:51:31 -0400

    changing app file for secret

--- app/HelloWorld.js
+++ app/HelloWorld.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello world with GitHub Actions?');
+app.get("/", (req, res) => {
+  res.send(process.env.GREETING || "Hello default");
 });
 
 app.listen(port, () => {
